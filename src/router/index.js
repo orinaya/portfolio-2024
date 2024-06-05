8c49c21e62a42ff5862835af8ecdf634c500781f
orianefrn <orianefrn@gmail.com>
2024-06-05 23:51:31 +0200

    fix pdf test

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const router = createRouter({
     },
   ],
 
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior() {
     return {top: 0}
   },
 })
--- src/views/ProjectsDetailsView.vue
+++ src/views/ProjectsDetailsView.vue
@@ -18,7 +18,7 @@ const getIconUrl = (image) => {
 }
 
 const pdfUrl = (pdf) => {
-  return new URL(`/works/${pdf}.pdf`, import.meta.url).href
+  return new URL(`@/assets/works/${pdf}.pdf`, import.meta.url).href
 }
 const router = useRouter()
 const userRoute = router.currentRoute.value.params.path
