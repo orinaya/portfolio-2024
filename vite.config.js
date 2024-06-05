04ae96152a7901c7389c75e117e2d6dbba94e3fb
orianefrn <orianefrn@gmail.com>
2024-06-05 23:54:27 +0200

    add vercel and change vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ import vue from "@vitejs/plugin-vue"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
